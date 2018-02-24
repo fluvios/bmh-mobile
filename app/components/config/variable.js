a1e56b8f7ded5dbac81860108a691d8b84a10181
fachri <fachri@mediawave.co.id>
2018-02-25 06:28:05 +0700

    update setting

--- app/components/config/variable.js
+++ app/components/config/variable.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import { AsyncStorage } from 'react-native';
-import Storage from 'react-native-storage';
+import React from 'react'
+import { AsyncStorage } from 'react-native'
+import Storage from 'react-native-storage'
 
 // Define API URL
-export const baseUrl = "http://bdv-hostmaster.com/";
-export const wpUrl = "http://www.bmh.or.id/wp-json/wp/v2/";
-
-// Define Storage
-// export var bmhStorage = new Storage({
-//     size: 1000,
-//     storageBackend: AsyncStorage,
-//     defaultExpires: null,
-//     enableCache: true,
-// })
+export const baseUrl = "http://bdv-hostmaster.com/"
+export const wpUrl = "http://www.bmh.or.id/wp-json/wp/v2/"
